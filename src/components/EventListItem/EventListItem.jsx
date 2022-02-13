0b2e925fe518e8acbeefb280399d467f5dbf5e32
Joaquin Azua <joaquin.diego.azua@gmail.com>
2022-02-12 18:42:58 -0600

    add some css

--- src/components/EventListItem/EventListItem.jsx
+++ src/components/EventListItem/EventListItem.jsx
@@ -4,15 +4,13 @@ import { Link } from 'react-router-dom'
 
 export default function EventListItem({event}) {
     var date = new Date(event.date);  
-    var d = date.getDate();
-    var m = date.getMonth()+1;
-    var y = date.getFullYear();
+    var d = date.toLocaleString();
 
     return (
         <div className="EventListItem">
             
             <div className="image "><img src={event.image} /></div>
-            <div className="name">{event.name} <hr /> Saturday {m}/{d}</div>
+            <div className="name"><span>{event.name} <hr /> Saturday {d}</span></div>
             <div className="buy">
                 <span></span>
                 <button className="buttn" >
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -33,6 +33,12 @@ function App() {
         :
         <AuthPage user={user} setUser={setUser} />
       }
+          <div className='footerinfo'>
+            <div className='hours'>Hours Of Operation<hr />MON. 11AM-6PM<br />TUES. 11AM-6PM<br />WED. 11AM-CLOSE<br />THUR. 11AM-CLOSE<br />FRI. 11AM-CLOSE<br />SAT. 11AM-CLOSE<br />SUN. 12PM-6PM<br /></div>
+            <div className='footernav'>Navigation</div>
+            <div className='hinfo'>Information <hr /> 2520 RODEO PLAZA <br /> FORT WORTH, TX 76164 <br /> (817)624-7117</div>
+          </div>
+            <footer className='copyright'>© ENCORE EVENTS • ALL RIGHTS RESERVED &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; WEBSITE DESIGN & DEVELOPEMENT BY Joaquin Azua </footer>
     </main>
   );
 }
